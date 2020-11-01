9c7146e3930cedfa3d88686d76eddc50e7c013ba
Markus Nuuja <markus.nuuja@tuni.fi>
2020-11-01 15:16:46 +0200

    added City class

---
+++ Game/city.hh
@@ -0,0 +1,26 @@
+#ifndef CITY_HH
+#define CITY_HH
+#include "interfaces/icity.hh"
+
+
+
+class City : public Interface::ICity
+{
+public:
+    City();
+    void setBackground(QImage& basicbackground, QImage& bigbackground) ;
+    void setClock(QTime clock) ;
+    void addStop(std::shared_ptr<Interface::IStop> stop) ;
+    void startGame();
+    void addActor(std::shared_ptr<Interface::IActor> newactor)  ;
+    void removeActor(std::shared_ptr<Interface::IActor> actor)  ;
+    void actorRemoved(std::shared_ptr<Interface::IActor> actor)  ;
+    bool findActor(std::shared_ptr<Interface::IActor> actor) const  ;
+    void actorMoved(std::shared_ptr<Interface::IActor> actor)  ;
+    std::vector<std::shared_ptr<Interface::IActor> > getNearbyActors(Interface::Location loc) const  ;
+    bool isGameOver() const ;
+public slots:
+
+};
+
+#endif // CITY_HH
