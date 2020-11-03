ffea66377f0c1a3e963c35d7e428974df634535b
jaakko <jaakko.raina@tuni.fi>
2020-11-03 21:37:14 +0200

    Remove own timer for 15sec bug

--- Game/creategame.cc
+++ Game/creategame.cc
@@ -4,14 +4,6 @@
 
 std::shared_ptr<Interface::ICity> Interface::createGame()
 {
-    while(1) {
-        qDebug() << QTime::currentTime().second();
-        if(QTime::currentTime().second() % 15 == 0){
-            break;
-        }else{
-            _sleep(1000);
-        }
-    }
     CourseSide::Logic gamelogic;
     gamelogic.fileConfig();
     std::shared_ptr<City> citypointer = std::make_shared<City>();
