8138160881d51f7e7dc570bd8b1363493157f02a
jaakko <jaakko.raina@tuni.fi>
2020-11-14 18:49:33 +0200

    Change passenger from pin to tonttu

--- Game/passangeruiitem.cpp
+++ Game/passangeruiitem.cpp
@@ -3,9 +3,9 @@
 PassangerUiItem::PassangerUiItem(int x, int y):ImgActorItem( x,  y)
 {
 
-    assert(pixmap_.load(":/img/pin_red16.png")); //assert that png file is loaded
+    assert(pixmap_.load(":/img/passenger.png")); //assert that png file is loaded
     setOffset(-8,-16);
-    setScale(1);
+    setScale(0.4);
 
     //QPixmap pixmapItems = pixmapItem.scaled(QSize(512,512),  Qt::KeepAspectRatio);
     setPixmap(pixmap_);
