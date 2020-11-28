11e94d9fc777c32b80c09acdde7aceae712f5519
jaakko <jaakko.raina@tuni.fi>
2020-11-28 19:11:36 +0200

    Add: setupdialog.h comments

--- Game/ui/setupdialog.h
+++ Game/ui/setupdialog.h
@@ -8,16 +8,33 @@ namespace Ui {
 class SetupDialog;
 }
 
+/**
+ * @brief The SetupDialog class creates dialog where user have to choose game
+ * starting parameters
+ */
 class SetupDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief SetupDialog default constructor
+     * @param parent is a QWidget pointer that defaults to nullptr
+     */
     explicit SetupDialog(QWidget *parent = nullptr);
+    /**
+     * @brief Destructor.
+     */
     ~SetupDialog();
 
 public slots:
+    /**
+     * @brief emitSettings emits settings signal
+     */
     void emitSettings();
+    /**
+     * @brief showRules start rulesdialog and show rules on it
+     */
     void showRules();
 
 signals:
