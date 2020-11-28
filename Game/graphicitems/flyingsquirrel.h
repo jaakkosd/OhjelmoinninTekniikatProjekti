651c8047ef0b9b8c71b86455aa0693a6067ef317
jaakko <jaakko.raina@tuni.fi>
2020-11-28 18:48:03 +0200

    Add: flyingsquirrel.h commented

--- Game/graphicitems/flyingsquirrel.h
+++ Game/graphicitems/flyingsquirrel.h
@@ -4,12 +4,26 @@
 #include "imgactoritem.h"
 
 namespace Game {
+/**
+ * @brief The FlyingSquirrel class creates ui implementation for flying squirrel
+ */
 class FlyingSquirrel : public ImgActorItem
 {
 public:
+    /**
+     * @brief FlyingSquirrel default constructor
+     * @param x actor's x coordinate
+     * @param y actor's y coordinate
+     */
     FlyingSquirrel(int x, int y);
 
 public slots:
+    /**
+     * @brief move squirrel to given position
+     * @param x new x coordinate
+     * @param y new y coordinate
+     * @return pair of coordinates
+     */
     QPair<int, int> move(int x, int y);
 private:
     QPixmap squirrelUp;
