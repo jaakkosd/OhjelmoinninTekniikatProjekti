bef7117d7c112ba19b0f960bbc1afba2937ff7d2
jaakko <jaakko.raina@tuni.fi>
2020-11-28 18:53:46 +0200

    Add: ratikkaitem.h comments

--- Game/graphicitems/ratikkaitem.h
+++ Game/graphicitems/ratikkaitem.h
@@ -3,10 +3,24 @@
 
 #include "imgactoritem.h"
 namespace Game {
+/**
+ * @brief The RatikkaItem class creates ui implementation for ratikka
+ */
 class RatikkaItem : public ImgActorItem
 {
 public:
+    /**
+     * @brief RatikkaItem default constructor.
+     * @param x ratikka x coordinate
+     * @param y ratikka y coordinate
+     */
     RatikkaItem(int x=0, int y=0);
+    /**
+     * @brief move moves ratikka to given position
+     * @param x ratikka new x coordinate
+     * @param y raeikka new y coordinate
+     * @return pair of x and y coordinate
+     */
     QPair<int,int> move(int x, int y);
 private:
     QPixmap ratikkaleft;
