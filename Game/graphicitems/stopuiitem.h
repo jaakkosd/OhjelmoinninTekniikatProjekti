95beafcf2edf695be520bf54e6e195a0e04aa09c
jaakko <jaakko.raina@tuni.fi>
2020-11-28 18:56:13 +0200

    Add: stopuiitem.h commented

--- Game/graphicitems/stopuiitem.h
+++ Game/graphicitems/stopuiitem.h
@@ -3,9 +3,17 @@
 
 #include "imgactoritem.h"
 namespace Game {
+/**
+ * @brief The StopUiItem class creates ui implementation for stops
+ */
 class StopUiItem : public ImgActorItem
 {
 public:
+    /**
+     * @brief StopUiItem default constructor
+     * @param x stop x coordinate
+     * @param y stop y coordinate
+     */
     StopUiItem(int x,int y);
 private:
     QPixmap pixmap_;
