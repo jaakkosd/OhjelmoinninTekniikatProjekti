8cf15ce52d840e8af2f71ac6a9077b32b9aaf4c5
jaakko <jaakko.raina@tuni.fi>
2020-11-28 18:59:32 +0200

    Add: comments to busuiitem

--- Game/graphicitems/busuiitem.h
+++ Game/graphicitems/busuiitem.h
@@ -5,10 +5,23 @@
 #include <QDebug>
 #include <QtMath>
 namespace Game {
+/**
+ * @brief The BusUiItem class creates ui implementation for bus
+ */
 class BusUiItem : public ImgActorItem
 {
 public:
+    /**
+     * @brief BusUiItem default constructor
+     * @param x bus x coordinate
+     * @param y bus y coordinate
+     */
     BusUiItem(int x, int y);
+    /**
+     * @brief moveTo move bus to given postion
+     * @param x bus new x coordinate
+     * @param y bus new y coordinate
+     */
     void moveTo(int x,int y);
 private:
     QPixmap pixmapLeft_;
