a6706301e5389522fd92ac526775c4fd6565b471
jaakko <jaakko.raina@tuni.fi>
2020-11-28 19:06:05 +0200

    Add: rulesdialog.h comments

--- Game/ui/rulesdialog.h
+++ Game/ui/rulesdialog.h
@@ -4,15 +4,25 @@
 #include <QDialog>
 
 namespace Ui {
+
 class RulesDialog;
 }
-
+/**
+ * @brief The RulesDialog class creates dialog which show rules of the game
+ */
 class RulesDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief RulesDialog default constructror
+     * @param parent is a QWidget pointer that defaults to nullptr
+     */
     explicit RulesDialog(QWidget *parent = nullptr);
+    /**
+     * @brief Destructor.
+     */
     ~RulesDialog();
 
 private:
