55e7edd9421d07eb6d7ae034ae208e9f07b37025
jaakko <jaakko.raina@tuni.fi>
2020-11-28 18:57:40 +0200

    Add: passangeruiitem.h comments

--- Game/graphicitems/passangeruiitem.h
+++ Game/graphicitems/passangeruiitem.h
@@ -2,9 +2,17 @@
 #define PASSANGERUIITEM_H
 #include "imgactoritem.h"
 namespace Game {
+/**
+ * @brief The PassangerUiItem class creates ui implementation for passenger
+ */
 class PassangerUiItem : public ImgActorItem
 {
 public:
+    /**
+     * @brief PassangerUiItem default constructor
+     * @param x passanger x coordinate
+     * @param y passanger y coordinate
+     */
     PassangerUiItem(int x, int y);
 private:
     QPixmap pixmap_;
